790513d4ad3c2039d0d34bcf6df867cdd19a9c85
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2021-06-23 11:42:48 -0400

    License Removal for O365 licenses in Azure AD
    Loop through an array of users and remove all licenses

---
+++ License removal Batch.ps1
@@ -0,0 +1,29 @@
+﻿$removal = @("user emails")
+
+
+
+
+
+
+
+foreach ($i in $removal){
+
+
+	$userUPN= $i
+	$userList = Get-AzureADUser -ObjectID $userUPN
+	$Skus = $userList | Select -ExpandProperty AssignedLicenses | Select SkuID
+		if($userList.Count -ne 0) {
+    			if($Skus -is [array])
+    		{
+        		$licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+        		for ($i=0; $i -lt $Skus.Count; $i++) {
+            		$Licenses.RemoveLicenses +=  (Get-AzureADSubscribedSku | Where-Object -Property SkuID -Value $Skus[$i].SkuId -EQ).SkuID   
+        	}
+        	Set-AzureADUserLicense -ObjectId $userUPN -AssignedLicenses $licenses
+    	} else {
+        	$licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+        	$Licenses.RemoveLicenses =  (Get-AzureADSubscribedSku | Where-Object -Property SkuID -Value $Skus.SkuId -EQ).SkuID
+       		Set-AzureADUserLicense -ObjectId $userUPN -AssignedLicenses $licenses
+    		}
+	}
+}
