4af9d5b606241eef362ced7ab76ded9d8bf3be90
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2021-06-24 10:05:10 -0400

    Add files via upload

---
+++ Export group members.ps1
@@ -0,0 +1,6 @@
+﻿
+Add-Type -AssemblyName Microsoft.VisualBasic
+$userID = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter your ADM username")
+$GroupName = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the name of the AD group to get membership")
+Get-ADGroupmember -identity "$GroupName" | select name | Export-Csv -path C:\Users\$userID\Desktop\Results.csv
+$FileLocation = [Microsoft.VisualBasic.Interaction]::MsgBox("C:Users\$userID\Desktop\Results.csv ")
